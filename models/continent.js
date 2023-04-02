888fba2a16dc65812c6aa341c6a14a15854c8952
Olga Bavykina <ozabotkina2@yandex.ru>
2023-04-02 16:14:18 +0300

    5

--- models/continent.js
+++ models/continent.js
@@ -14,63 +14,63 @@ const continentSchema = new mongoose.Schema({
     required: true,
     unique: true,
     index: true,
-    validate: {
-      validator(v) {
-        return regContinentCode.test(v);
-      },
-      message: () => `2 latin caps`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regContinentCode.test(v);
+    //   },
+    //   message: () => `2 latin caps`,
+    // },
   }, 
 
   title: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexRus.test(v);
-      },
-      message: () => `русские буквы и цифры`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regexRus.test(v);
+    //   },
+    //   message: () => `русские буквы и цифры`,
+    // },
   },
 
   coverTitle: {
     type: String,
-    validate: {
-      validator(v) {
-        return regCoverTitle.test(v);
-      },
-      message: () => `русские буквы и цифры до 25`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regCoverTitle.test(v);
+    //   },
+    //   message: () => `русские буквы и цифры до 25`,
+    // },
   },
 
   titleEn: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexLatin.test(v);
-      },
-      message: () => `латинские буквы и цифры`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regexLatin.test(v);
+    //   },
+    //   message: () => `латинские буквы и цифры`,
+    // },
   },
 
 
   coverTitleEn: {
     type: String,
-    validate: {
-      validator(v) {
-        return regCoverTitleEn.test(v);
-      },
-      message: () => `латинские буквы и цифры до 25`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regCoverTitleEn.test(v);
+    //   },
+    //   message: () => `латинские буквы и цифры до 25`,
+    // },
   },
 
   picture: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexLink.test(v);
-      },
-      message: () => `ссылка`,
-    },
+    // validate: {
+    //   // validator(v) {
+    //   //   return regexLink.test(v);
+    //   // },
+    //   message: () => `ссылка`,
+    // },
   },
 
   // countries: 
--- models/trip.js
+++ models/trip.js
@@ -15,62 +15,62 @@ const tripSchema = new mongoose.Schema({
     required: true,
     unique: true,
     index: true,
-    validate: {
-      validator(v) {
-        return regTripCode.test(v);
-      },
-      message: () => `2-3 latin caps, 2 digits`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regTripCode.test(v);
+    //   },
+    //   message: () => `2-3 latin caps, 2 digits`,
+    // },
   }, 
 
   title: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexRus.test(v);
-      },
-      message: () => `русские буквы и цифры`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regexRus.test(v);
+    //   },
+    //   message: () => `русские буквы и цифры`,
+    // },
   },
 
   coverTitle: {
     type: String,
-    validate: {
-      validator(v) {
-        return regCoverTitle.test(v);
-      },
-      message: () => `русские буквы и цифры до 25`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regCoverTitle.test(v);
+    //   },
+    //   message: () => `русские буквы и цифры до 25`,
+    // },
   },
 
   titleEn: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexLatin.test(v);
-      },
-      message: () => `латинские буквы и цифры`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regexLatin.test(v);
+    //   },
+    //   message: () => `латинские буквы и цифры`,
+    // },
   },
 
   coverTitleEn: {
     type: String,
-    validate: {
-      validator(v) {
-        return regCoverTitleEn.test(v);
-      },
-      message: () => `латинские буквы и цифры до 25`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regCoverTitleEn.test(v);
+    //   },
+    //   message: () => `латинские буквы и цифры до 25`,
+    // },
   },
 
   picsPath: {
     type: String,
-    validate: {
-      validator(v) {
-        return regexLink.test(v);
-      },
-      message: () => `ссылка`,
-    },
+    // validate: {
+    //   validator(v) {
+    //     return regexLink.test(v);
+    //   },
+    //   message: () => `ссылка`,
+    // },
   },
 
   coverPic: {
